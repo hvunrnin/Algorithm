09819971aa91da75f70d23a4bb8370cbf55d9bd6
hvunrnin <124487925+hvunrnin@users.noreply.github.com>
2024-06-23 23:25:33 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[10];
+int input[10];
+int used[10];
+int m,n;
+
+void func(int k){
+    if(k==m){
+        for(int i=0;i<m;i++){
+            cout<<input[arr[i]]<<" ";
+        }
+        cout<<"\n";
+    }
+    int start=0;
+    if(k!=0) start=arr[k-1]+1;
+    for(int i=start;i<n;i++){
+        if(!used[i]){
+            arr[k]=i;
+            used[i]=true;
+            func(k+1);
+            used[i]=false;
+        }
+    }
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        int num=0;
+        cin>>num;
+        input[i]=num;
+    }
+    sort(input, input+n);
+    func(0);
+}
