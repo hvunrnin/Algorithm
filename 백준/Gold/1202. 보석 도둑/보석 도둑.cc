bd21c366102488a02b60cae2ae5a1da8415892e7
Hyunmin Kwon <124487925+hvunrnin@users.noreply.github.com>
2024-08-07 15:59:45 +0900

    [Gold II] Title: 보석 도둑, Time: 240 ms, Memory: 18492 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -2,35 +2,32 @@
 #include <set>
 #include <utility>
 #include <algorithm>
+
 using namespace std;
 
-#define X first
-#define Y second
-
-int n, k;
-pair<int, int> jewel[300000]; 
+int n,k;
+pair<int,int> arr[300000];
 multiset<int> bag;
 
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> k;
-  for(int i = 0; i < n; i++)
-    cin >> jewel[i].Y >> jewel[i].X;
-  sort(jewel, jewel+n);
-  for(int i = 0; i < k; i++){
-    int c;
-    cin >> c;
-    bag.insert(c);
-  }
-  long long ans = 0;
-  for(int i = n-1; i >= 0; i--){
-    int m, v;
-    tie(v, m) = jewel[i];
-    auto it = bag.lower_bound(m);
-    if(it == bag.end()) continue;
-    ans += v;
-    bag.erase(it);
-  }
-  cout << ans;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+        cin>>arr[i].second>>arr[i].first;
+    }
+    sort(arr,arr+n);
+    while(k--){
+        int num;
+        cin>>num;
+        bag.insert(num);
+    }
+    long long ans=0;
+    for(int i=n-1;i>=0;i--){
+        auto b=bag.lower_bound(arr[i].second);
+        if(b==bag.end()) continue;
+        ans+=arr[i].first;
+        bag.erase(b);
+    }
+    cout<<ans;
 }
