6955c4fcff51f66484ec4df170829733dd012f51
Hyunmin Kwon <124487925+hvunrnin@users.noreply.github.com>
2024-08-14 20:09:19 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.cc
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -1,73 +1,56 @@
-// 한 명씩 저장
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-
+// Authored by : scsc3204
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/ac7620713b584538a952a5d919324cbb
+#include <bits/stdc++.h>
 using namespace std;
 
 int n;
-int arr[51][51];
-int ans[51];
-vector<int> v;
-vector<int> adj[51];
-bool vis[51];
+vector<int> adj[52];
+vector<pair<int, int>> candsc;
+int score[52]; // 각 사람의 점수
 
-int bfs(int num){
-    fill(vis,vis+50,0);
-    queue<int> q;
-    //q.push(num);
-    vis[num]=true;
-    for(auto c:adj[num]){
-        q.push(c);
-        vis[c]=true;
+int bfs(int st){ // st의 점수 계산
+  queue<int> q;
+  int dist[52];
+  fill(dist, dist+n+1, -1);
+
+  dist[st] = 0;
+  q.push(st);
+  while(!q.empty()) {
+    int cur = q.front(); q.pop();
+    for(int fr : adj[cur]) {
+      if(dist[fr] != -1) continue;
+      dist[fr] = dist[cur] + 1;
+      q.push(fr);
     }
-    while(!q.empty()){
-        int cur=q.front();
-        q.pop();
-        for(auto c:adj[cur]){
-            if(arr[num][c]!=55||vis[c]) continue;
-            arr[num][c]=arr[num][cur]+1;
-            q.push(c);
-            vis[c]=true;
-        }
-    }
-    return *max_element(arr[num],arr[num]+n+1);
+  }
+  int val = *max_element(dist + 1, dist + n + 1); // 가장 거리가 먼 사람
+  if(val == 0) return 100; // 친구가 아예 없으면 val은 0이 됨
+  return val;
 }
 
-int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        fill(arr[i],arr[i]+n+1,55);
-        arr[i][0]=0;
-        arr[i][i]=0;
-    }
-    int num1=0,num2=0;
-    while(1){
-        cin>>num1;
-        cin>>num2;
-        if(num1==-1&&num2==-1) break;
-        arr[num1][num2]=1;
-        arr[num2][num1]=1;
-        adj[num1].push_back(num2);
-        adj[num2].push_back(num1);
-    }
-    for(int i=1;i<=n;i++){
-        ans[i]=bfs(i);
-    }
-    cout<<*min_element(ans+1,ans+n+1)<<" ";
-    int cnt=0;
-    for(int i=1;i<=n;i++){
-        if(ans[i]==*min_element(ans+1,ans+n+1)){
-            cnt++;
-            v.push_back(i);
-        }
-    }
-    cout<<cnt<<"\n";
-    sort(v.begin(),v.end());
-    for(auto c:v){
-        cout<<c<<" ";
-    }
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  int u = 0, v = 0;
+  while(u != -1 && v != -1) {
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  for(int i = 1; i <= n; i++)
+    score[i] = bfs(i);
+  
+  int mnscore = *min_element(score+1, score+n+1);
+  vector<int> cand; // 후보 목록
+  for(int i = 1; i <= n; i++){
+    if(score[i] == mnscore)
+      cand.push_back(i);
+  }
+
+  cout << mnscore << ' ' << cand.size() << '\n';
+  for(int c : cand) cout << c << ' ';
 }
